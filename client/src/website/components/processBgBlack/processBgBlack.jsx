a952f2f7fd6bb529d5257bc31347d4b310bc9b11
LENOVO LAPTOP <Earningplanerinstitute@gmail.com>
2025-03-02 15:37:58 +0530

    sdfsdf

--- client/src/website/components/processBgBlack/processBgBlack.jsx
+++ client/src/website/components/processBgBlack/processBgBlack.jsx
@@ -4,7 +4,7 @@ import { FaSpinner } from 'react-icons/fa';
 
 const ProcessBgBlack = () => {
     return createPortal(
-        <div className='fixed inset-0 flex justify-center items-center bg-black bg-opacity-60 z-50'>
+        <div className='fixed inset-0 flex justify-center items-center bg-black bg-opacity-60 z-50 select-none'>
             <div className='flex flex-col items-center justify-center p-6 bg-white rounded-2xl shadow-lg'>
                 <FaSpinner className='text-blue-600 text-5xl animate-spin' />
                 <p className='text-gray-800 text-lg font-semibold mt-4'>Processing...</p>
--- client/src/website/components/processBgSeprate/processBgSeprate.jsx
+++ client/src/website/components/processBgSeprate/processBgSeprate.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProcessBgSeprate = () => {
   return (
-    <div className="w-full h-full inset-0 flex items-center justify-center bg-white bg-opacity-80">
+    <div className="w-full h-full inset-0 flex items-center justify-center bg-white bg-opacity-80 select-none">
       <div className="bg-white p-6 rounded-2xl shadow-lg flex flex-col items-center text-center">
         <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
         <h2 className="text-xl font-bold mt-4 text-gray-800">Loading...</h2>
