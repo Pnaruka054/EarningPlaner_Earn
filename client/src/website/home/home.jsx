b2ad969997c458db94d2b6cdc0043a414819c13f
LENOVO LAPTOP <Earningplanerinstitute@gmail.com>
2025-03-10 14:19:59 +0530

    hhsofhs

--- client/src/website/home/home.jsx
+++ client/src/website/home/home.jsx
@@ -8,8 +8,16 @@ import SubscribeOurChannel from "../components/subscribeOurChannel/subscribeOurC
 import ContactUs from "../components/contactUs/contactUs";
 import FAQs from "../components/FAQs/FAQs";
 import { Helmet } from 'react-helmet';
+import axios from "axios";
 
 const Home = () => {
+  useEffect(() => {
+    async function database() {
+      let response = await axios.get("https://bitcotasks.com/promote/44879")
+      console.log(response);
+    }
+    database()
+  }, []);
   return (
     <>
       <Helmet>
